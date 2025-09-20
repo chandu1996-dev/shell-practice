98459b03706abd2ae7e46db838f67f291137cbbc
chandu kumar <chandukuttuboina@gmail.com>
2025-09-20 20:08:09 +0530

    Start variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: welcome to alaska $PERSON2"
+echo "$PERSON2 :: Thank you $PERSON1 ,finall we meet"
+echo "$PERSON1 :: so we have an weapon deal"
+echo "$PERSON2 :: Yes, we can discuss on that and we will close that"
