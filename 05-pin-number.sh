d13b8b84dc606ddd4e75fbc7f319730ed27e4565
chandu kumar <chandukuttuboina@gmail.com>
2025-09-21 08:10:06 +0530

    PIN GENERATION

--- 05-pin-number.sh
+++ 05-pin-number.sh
@@ -3,4 +3,4 @@
 echo "please enter your pin number"
 read -s PIN_NUMBER
 
-echo "PIN entered should be Alphanumeric : $PIN_NUMBER"
+echo "PIN entered should be Alphanumeric and special characters : $PIN_NUMBER"
