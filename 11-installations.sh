09e5bbc899942a00bf5371f25a974148a66bc7e9
chandu kumar <chandukuttuboina@gmail.com>
2025-09-23 00:27:19 +0530

    installataion

--- 11-installations.sh
+++ 11-installations.sh
@@ -5,15 +5,30 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
 
 echo "Error :: please run the script with root previlage"
-exit 1 #
+exit 1 #failure is other than 0
 fi
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+    echo "ERROR :: MYSQL installation $2 failure"
+    exit 1
+else 
+        echo "MYSQL installation $2 success"
+
+fi 
+
+}
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]; then
-    echo "ERROR :: MYSQL installation failure"
-    exit 1
-else 
-        echo "MYSQL installation success"
+VALIDATE $? "MYSQL"
 
-fi 
+
+dnf install nignx -y
+VALIDATE $? "nignx"
+
+
+#dnf install mongodb-mongosh -y
+#VALIDATE $? "mongodb"
+
