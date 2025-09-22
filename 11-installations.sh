5e85305b64823d69a62bbc59582be170f44c25c5
chandu kumar <chandukuttuboina@gmail.com>
2025-09-23 00:02:50 +0530

    installataion

--- 11-installations.sh
+++ 11-installations.sh
@@ -5,14 +5,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
 
 echo "Error :: please run the script with root previlage"
-
+exit 1 #
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR :: MYSQL installation failure"
+    exit 1
 else 
         echo "MYSQL installation success"
 
-fi
+fi 
