a5975c85db0e00f966798d181105b0c8e1cfe3bd
chandu kumar <chandukuttuboina@gmail.com>
2025-09-23 21:57:15 +0530

    Loops

---
+++ 14-Loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+Logs_folder="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/12-logs.log
+echo "Script started executed at: $(date)" 
+
+mkdir -p $Logs_folder
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
+}
+
+#$@ --> to pass all arguments to the script
+
+
+for package  in $@
+do
+    echo "package is : $package"
+done
