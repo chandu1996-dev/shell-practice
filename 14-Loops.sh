718fc5e14541572a4c3729389ef1ae000adeeace
chandu kumar <chandukuttuboina@gmail.com>
2025-09-23 22:44:42 +0530

    Loops

--- 14-Loops.sh
+++ 14-Loops.sh
@@ -6,12 +6,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-Logs_folder="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/12-logs.log
-echo "Script started executed at: $(date)" 
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/14-Loops.log
 
-mkdir -p $Logs_folder
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
@@ -20,27 +20,25 @@ fi
 
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 ... $R FAILURE $N"
+        echo -e "Installing $2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "Installing $2 ... $G SUCCESS $N"
+        echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-#$@ --> to pass all arguments to the script
+# $@
 
-
-for package  in $@
+for package in $@
 do
-    #check package is already installed or not
+    # check package is already installed or not
     dnf list installed $package &>>$LOG_FILE
 
-        if [ $? -ne 0 ]; then
-            dnf install $package -y &>>$LOG_FILE
-            VALIDATE $? "$package"
-
-        else
-        echo -e "$package already installed .. $Y SKIPPING $N" 
-        fi
-
+    # if exit status is 0, already installed. -ne 0 need to install it
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "$package already installed ... $Y SKIPPING $N"
+    fi
 done
